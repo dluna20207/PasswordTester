fb8a399784ae3bf7a81ff02ab0c89bd3dde98d50
Luna Daniel <20207@columbushs.com>
2020-02-11 09:40:13 -0500

    Password is checking length

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
-function myFunction() {
-    var x = document.getElementById("passwordInput");
+function passwordVisible() {
+    let x = document.getElementById("passwordInput");
     if (x.type === "password") {
-      x.type = "text";
+        x.type = "text";
     } else {
-      x.type = "password";
+        x.type = "password";
     }
-  }
+}
+
+function strengthCheck() {
+    let passwordValue = document.getElementById("passwordInput").value;
+    let strengthValue = 0;
+
+    if (passwordValue.length >= 20) {
+        strengthValue += 1;
+    }
+
+
+
+    if(passwordValue.length == 0){
+        document.getElementById("strengthOutput").innerHTML = "Please Enter Password";
+    }
+    else if(strengthValue == 0){
+        document.getElementById("strengthOutput").innerHTML = "Your Password is Extremely Weak";
+    }
+    else if(strengthValue == 1){
+        document.getElementById("strengthOutput").innerHTML = "Your Password is Weak";
+    }
+}
