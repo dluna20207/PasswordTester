9ee072971eccbf959dba8721d2cfe22b5161c7ef
Luna Daniel <20207@columbushs.com>
2020-02-13 12:12:45 -0500

    upper and lower case is being checked correctly

--- script.js
+++ script.js
@@ -11,19 +11,52 @@ function strengthCheck() {
     let passwordValue = document.getElementById("passwordInput").value;
     let strengthValue = 0;
 
-    if (passwordValue.length >= 20) {
-        strengthValue += 1;
+    if (passwordValue.length >= 15) {
+        strengthValue++;
+    }
+    if (passwordValue.length >= 25) {
+        strengthValue++;
+    }
+
+    for (var i = 0; i < passwordValue.length; i++) {
+        let numOfLower = 0;
+        let character = String(passwordValue.charAt(i))
+        if (character == character.toLowerCase() && isNaN(character)) {
+            numOfLower++;
+        }
+        if (numOfLower > 0) {
+            strengthValue++;
+            break;
+        }
+    }
+
+    for (var i = 0; i < passwordValue.length; i++) {
+        let numOfUpper = 0;
+        let character = String(passwordValue.charAt(i))
+        if (character == character.toUpperCase() && isNaN(character)) {
+            numOfUpper++;
+        }
+        if (numOfUpper > 0) {
+            strengthValue++;
+            break;
+        }
     }
 
 
 
-    if(passwordValue.length == 0){
+    if (passwordValue.length == 0) {
         document.getElementById("strengthOutput").innerHTML = "Please Enter Password";
     }
-    else if(strengthValue == 0){
+    else if (strengthValue == 0) {
         document.getElementById("strengthOutput").innerHTML = "Your Password is Extremely Weak";
     }
-    else if(strengthValue == 1){
+    else if (strengthValue == 1) {
         document.getElementById("strengthOutput").innerHTML = "Your Password is Weak";
+    }
+    else if (strengthValue == 2) {
+        document.getElementById("strengthOutput").innerHTML = "Your Password is Normal";
+    }
+    else if (strengthValue == 3) {
+        document.getElementById("strengthOutput").innerHTML = "Your Password is Somewhat Strong";
     }
 }
